4c01d1c580bc2f3cfe63890c8d277c2253a7b88e
Christopher Lucier <OneDudeDesign@gmail.com>
2021-01-26 14:04:46 -0800

    Step08: Added a ground overlay

--- app/src/main/java/com/onedudedesign/wander/MapsActivity.kt
+++ app/src/main/java/com/onedudedesign/wander/MapsActivity.kt
@@ -11,10 +11,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MapStyleOptions
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 import java.util.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -50,6 +47,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val homeLatLng = LatLng(latitude, longitude)
 
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
+
+        val overlaySize = 100f
+        val androidOverlay = GroundOverlayOptions()
+                .image(BitmapDescriptorFactory.fromResource(R.drawable.android))
+                .position(homeLatLng, overlaySize)
+
+        map.addGroundOverlay(androidOverlay)
 
         map.addMarker(MarkerOptions().position(homeLatLng))
 
